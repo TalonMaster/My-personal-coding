8ec9641bd58b7ca6ebab5db44a197e99750e4ee0
TalonMaster <davecraft122@gmail.com>
2025-07-26 18:33:35 +0100

    Hiiiiiiii

--- Nagi.js
+++ Nagi.js
@@ -1,6 +1,39 @@
-var a = 'David'
-var b = 'Kaiser'
-var c = 10
-var d = true
-var e = false
+function goToWashroom() {
+
+console.log("hi mam can i go to washroom")
+
+console.log("please mam, allow me to go please")
+
+}
+
+goToWashroom()
+
+goToWashroom()
+
+goToWashroom()
+
+function add(a,b,c,d){
+
+var result = a + b + c + d
+
+return result
+
+}
+
+var a = add(4,6,10,20)
+
+console.log(a)
+
+var b = add(55,11,999,123)
+
 console.log(b)
+
+function multiply(a,b,c){
+
+return a * b * c
+
+}
+
+var c = multiply(80,100,230)
+
+console.log(c)
